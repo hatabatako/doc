1c6520b05e227ca5e8a5ded52fcfa5f3f6986211
batako <hatabata.5296@gmail.com>
2019-12-18 19:33:26 +0900

    アイコンのくり抜き方がわかりません・・・！

--- js/main.js
+++ js/main.js
@@ -22,6 +22,10 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 	//scroll
 	var section = $('main > section');
 
+	//windowロード
+
+
+
 	//window(表示されているところ)がscroll
 	//検索キーワード「onイベント jquery」
 	$(window).on('scroll',function(){
@@ -85,5 +89,35 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 	});
 
 
+
+	// ページ全体の読込完了後、またはリサイズ後の処理
+	$(window).on('load resize', function(){
+		var menuImg = $('#menu_list > img');	// メニュー画面の画像指定を変数化
+		var imgWidth = menuImg.width();			// 画像の幅を取得
+
+		var menuList = $('#menu_list > div');	// メニュー画面の項目全体のdiv指定を変数化
+		var windowWidth = $(window).width();	// 表示されている画面（window）の幅を取得
+		var windowHeight = menuImg.height();
+		menuList.css('position','relative');
+
+		if(windowWidth>=750){	// PC width 
+			var menuListwidth = windowWidth - imgWidth;
+			menuImg.width('auto');
+			menuList.width(menuListwidth);
+		}else{	// SP width
+			menuImg.width(windowWidth);
+			menuList.css('position','absolute');
+			menuList.height(windowHeight);
+			menuList.width(windowWidth);
+		}
+	});
+
+
+
+
+
+
+
+	
 
 })
