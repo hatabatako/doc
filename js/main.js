d01d740f17aaee5a47f4c7f5ddfd1c8a51162ab0
batako <hatabata.5296@gmail.com>
2020-06-19 19:22:53 +0900

    再コミット。New以下は無視してください。

--- js/main.js
+++ js/main.js
@@ -1,56 +1,74 @@
 $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 
-	$('#menu').on('click',function(){
+	// ＝＝＝＝　メニュー画面表示非表示設定　＝＝＝＝
+	$('#menu,#menu_list a').on('click',function(){
 		menu();
 	});
 
 
-	//scroll
-	 var section = $('main > section');
+	//メニュースクロール表示
+	$('a[href^="#"]').on('click',function(){
+		var speed = 400;//速度
+	    var href = $(this).attr("href");//場所
+    	var target = $(href == "#" || href == "" ? 'html' : href);
+    	var position = target.offset().top;
+    	$('html, body').animate({scrollTop:position}, speed, "swing");
+    	return false;
+	});
 
-	
-	//window(表示されているところ)がscroll
-	//検索キーワード「onイベント jquery」
+
+	//各sectionの情報を取得
+	var section = $('main > section');
+	// console.log(section);
+
+	//window(表示されている画面)がscrollしたら
 	$(window).on('scroll',function(){
 
-		//現在地の取得（Topからscrollした距離）
+		//現在地の取得(Topからscrollした距離)
 		var cur_pos = $(this).scrollTop();
 
-		//main直下のセクションすべてに対して実施(each)
+		//section全てに対して実施(each)
 		section.each(function(){
 
 			//各sectionのTopとBottomのy座標を取得
 			var top = $(this).offset().top;
+			//TopにSectionの高さを足す
 			var bottom = top + $(this).outerHeight();
 
-			//表示位置のずらし
+			//ずらし
 			var offset = 175;
 
-
-			//現在地(画面の一番上)が各セクションのTopからBottom(+offset)の間にいたとき実施
+			//現在地が各セクションのTopからBottom(+offset)の間にいるとき
 			if(cur_pos >= top - offset && cur_pos <= bottom - offset){
 
 				//activeクラスを全て除去
-				$('section#top').removeClass('active');
-				//現在地のセクションのIDを取得
+				$('section').removeClass('active');
+				$('div#background-image').removeClass('active');
+
+				//現在地のセクションIDを取得
 				var this_id = $(this).attr('id');
 
 				//対象セクションIDに対して、activeクラスをつける
 				switch(this_id){
 					case 'top':
 						$(this).addClass('active');
+						$('.bg_tp').addClass('active');
 						break;
 					case 'profile':
 						$(this).addClass('active');
+						$('.bg_pf').addClass('active');
 						break;
-					case 'works':
+					case 'work':
 						$(this).addClass('active');
+						$('.bg_wk').addClass('active');
 						break;
 					case 'blog':
 						$(this).addClass('active');
+						$('.bg_bg').addClass('active');
 						break;
 					case 'contact':
 						$(this).addClass('active');
+						$('.bg_ct').addClass('active');
 						break;
 					default:
 						break;
@@ -59,96 +77,33 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 			}
 
 
-			//セクション#topがアクティブクラスが付与されてなかったら、付与する
-			if(!$('section#top').hasClass('active')){
-				$('#background-image').addClass('active');
-			}else{
-				$('#background-image').removeClass('active');
-			}
 		});
 
-
 	});
 
 
-	// ページ全体の読込完了後、またはリサイズ後の処理
-	$(window).on('load resize', function(){
-		var menuImg = $('#menu_list > img');	// メニュー画面の画像指定を変数化
-		var imgWidth = menuImg.width();			// 画像の幅を取得
+	//バリデーションチェック
+	//blur：フォーカスが外れたタイミングで実行
+	$('input,textarea').on('blur',function(){
 
-		var menuList = $('#menu_list > div');	// メニュー画面の項目全体のdiv指定を変数化
-		var windowWidth = $(window).width();	// 表示されている画面（window）の幅を取得
-		var windowHeight = menuImg.height();
-		menuList.css('position','relative');
-
-		if(windowWidth>=750){	// PC width 
-			var menuListwidth = windowWidth - imgWidth;
-			menuImg.width('auto');
-			menuList.width(menuListwidth);
-		}else{	// SP width
-			menuImg.width(windowWidth);
-			menuList.css('position','absolute');
-			menuList.height(windowHeight);
-			menuList.width(windowWidth);
-		}
-	});
+		var val  =$(this).val(); //値を取得
+		var name =$(this).attr('name'); //nameを取得
+		var id   ='#' + name;//idを作成
+		console.log(val);
+		console.log(name);
+		console.log(id);
 
 
-	//メニュー画面飛ばす
-	$('a[href^="#"]').on('click',function(){
-		var speed=400;
-		var href=$(this).attr("href");//属性値の取得　attr('属性') name,typeとか
-		var cl=$(this).attr("class");
-		console.log(cl);
-		//(念のための)移動先を取得
-		var target = $(href == "#" || href == "" ? 'html' :href);
-		//移動先を数値を取得(セクションのトップの位置を取得)
-		var position = target.offset().top;
-
-		$('body,html').animate({scrollTop:position}, speed, 'swing');
-
-		if(href!='#' && cl !="scroll"){
-			menu();
-		}
-		return false;
-	});
-
-	//読み込み、リサイズしたら
-	$(window).on('load resize',function(){
-		//レスポンシブ　高さ
-		$('section').each(function(i,e){
-			//var height = e.height;
-			var height = $(this).height();
-			var windowHeight = $(window).outerHeight();
-
-
-			if(height <= windowHeight){
-				$('#'+ e.id).css('height','100vh');
-			}else{
-				$('#'+ e.id).css('height','auto');
-			}
-		});
-	});
-
-
-
-	//バリエーションチェック
-	//activeが外れた状態がblur onイベントで調べよう
-	$('input, textarea').on('blur' ,function(){
-		//入力値の確認 ログ出力
-		//console.log($(this).val());
-		var val =$(this).val();
-		var name =$(this).attr('name');
-		var id = '#' + name;
-
-		//NullCheck
-		if(nullChk(val)){
-			errMsg(id, '');
+		//NullCheck 値がNullだったら
+		if(nullChk(val)){ //Nullチェック関数
+			//エラーメッセージを空欄に上書き
+			errMsg(id,''); 
 		}else{
-			errMsg(id, '必須項目です');
-			return false; //処理中断
+			errMsg(id,'必須項目です');
+			return false;//処理中断
 		}
 
+		//電話、メール、半角記号チェック
 		if(name == 'tel'){ //電話形式チェック
 			if(telChk(val)){
 				errMsg(id,'');
@@ -157,9 +112,9 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 			}
 		}else if(name == 'email'){ //メール形式チェック
 			if(mailChk(val)){
-			 	errMsg(id,'');
+				errMsg(id,'');
 			}else{
-			 	errMsg(id,'メールの形式が異なります');
+				errMsg(id,'メールの形式が異なります');
 			}
 		}else{ //半角記号チェック
 			if(hlfChk(val)){
@@ -179,6 +134,8 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 		}
 	});
 
+
+
 	//Ajax
 
 	//$('.blg_list').html('') //HTMLの中身をクリア
@@ -190,29 +147,22 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 	})
 	.done(function(data){
 		console.log(data);
-
-		$('.blg_list').html('');
+		//blog_list内を空っぽにする
+		$('.blog_list').html('');
 		for(var i=0; i < data.length; i++){
-			//Title
-			//console.log("i="+i);
-			//console.log("datalength="+data.length);
-			//console.log(data[i].title.rendered);
-			$('.blg_list').append('<div class="blg_block"></div>');
-			var num = i + 1;
-			$('.blg_block:nth-of-type('+ num +')').append('<a href="#"></a>');
-			$('.blg_block:nth-of-type('+ num +')').append('<div class="blg_img" style="background-image:url('+data[i]['_embedded']['wp:featuredmedia'][0]['source_url']+')"></div>');
-			$('.blg_block:nth-of-type('+ num +')').append('<div class="blg_text"></div>');
-			$('.blg_block:nth-of-type('+ num +') > .blg_text').append('<p class="blg_title">'+data[i].title.rendered+'</p>');
-			$('.blg_block:nth-of-type('+ num +') > .blg_text').append('<p class="blg_more">more</p>');
-			//console.log(i+"Title="+data[i].title.rendered);
+			$('.blog_list').append('<div class="blog_box"></div>');
 
-			//$('.blg_block > .blg_text > .blg_title').append(data[i].title.rendered);
+			var num_block = i + 1;
+			$('.blog_box:nth-of-type('+ num_block +')').append('<a class="blog_link" href="#"></a>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link').append('<div class="blog_img"></div>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link').append('<div class="blog_text"></div>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link > .blog_img').append('<img src="'+data[i]['_embedded']['wp:featuredmedia'][0]['source_url']+'"></img>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link > .blog_text').append('<div class="blog_title"></div>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link > .blog_text').append('<div class="blog_memo"></div>')
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link > .blog_text > .blog_title').append('<p>'+data[i].title.rendered+'</p>');
+			$('.blog_box:nth-of-type('+ num_block +') > .blog_link > .blog_text > .blog_memo').append('<p>'+data[i].excerpt.rendered+'</p>');
 
-			//内容
-			//console.log(data[i].excerpt.rendered);
-			//$('.blg_block > .blg_text > .blg_more').append(data[i].title.rendered);
-			//アイキャッチ画像ID
-			//console.log(data[i]['_embedded']['wp:featuredmedia'][0]['source_url']);
+
 		}
 
 	})
@@ -224,42 +174,39 @@ $(function(){/*HTML&CSS(Dom)を読み込み完了後に実施*/
 
 });
 
-
+// メニュー画面表示非表示
 function menu(){
 	if($('#menu').hasClass('open')){
 		//openのとき
-		$('#menu_list').removeClass('first');
 		$('#menu').removeClass('open');
-		$('#menu_list').addClass('active');
+		$('#menu_list').removeClass('open');
 		$('#menu').addClass('close');
-		$('body').css('overflow','hidden');/*今表示されてる画面のみ認識*/
+		$('#menu_list').addClass('close');
 	}else{
 		$('#menu').removeClass('close');
-		$('#menu_list').removeClass('active');
+		$('#menu_list').removeClass('close');
 		$('#menu').addClass('open');
-		$('#menu_list').addClass('first');
-		$('body').css('overflow','visible');/*デフォルトの状態*/
+		$('#menu_list').addClass('open');
 	}
 }
 
-function removeClass(){
 
-}
-
+//=============================
+//    バリデーションチェック関数    
+//=============================
 //Null Check
 function nullChk(val){
-	//参考演算子 (条件式) ? 真の場合 : 偽の場合;
+	//三項演算子（条件式） ? 真の場合：偽の場合;
+	//値がNullまたは空白または変数定義がなかった場合
 	return (val == null || val == '' || val == undefined) ? false :true;
 }
 
 //Output Error Message
 function errMsg(id, msg){
-	//#name > p
-	//name > pをmsgに書き換える
 	$(id + ' > p').text(msg);
 }
 
-// Telephone Number Check
+//Telephone Number Check
 function telChk(val){
 	//\d{3,5}-\d{3,4}-\d{3,5}
 	/*
@@ -289,4 +236,3 @@ function errorAllChek(){
 	});
 	return cnt;
 }
-
